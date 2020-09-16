32b73f3ebf4d916e2dd5f146f15eaebbeb3fc356
Thiago Corta <thiago.corta@gmail.com>
2020-09-16 18:09:30 -0300

    cambiando estilos

--- src/app/componentes/cabecera/cabecera.component.ts
+++ src/app/componentes/cabecera/cabecera.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-cabecera',
-  templateUrl: './cabecera.component.html',
-  styleUrls: ['./cabecera.component.css']
+  selector: "app-cabecera",
+  templateUrl: "./cabecera.component.html",
+  styleUrls: ["./cabecera.component.css"],
 })
 export class CabeceraComponent implements OnInit {
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
