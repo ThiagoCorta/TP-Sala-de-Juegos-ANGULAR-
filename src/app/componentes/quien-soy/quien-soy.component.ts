e3aaa8781aadb3302197e6e6185a887aff155eb6
Thiago Corta <thiago.corta@gmail.com>
2020-09-17 19:10:24 -0300

    quien soy pagina,,

--- src/app/componentes/quien-soy/quien-soy.component.ts
+++ src/app/componentes/quien-soy/quien-soy.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-quien-soy',
-  templateUrl: './quien-soy.component.html',
-  styleUrls: ['./quien-soy.component.css']
+  selector: "app-quien-soy",
+  templateUrl: "./quien-soy.component.html",
+  styleUrls: ["./quien-soy.component.css"],
 })
 export class QuienSoyComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  navigateGithub() {
+    window.open("https://github.com/thiagocorta", "_blank");
   }
-
 }
